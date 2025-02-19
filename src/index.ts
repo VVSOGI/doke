ff2781888f9a273667f4f8b5ac598777de8ea622
benny <vvsogi@gmail.com>
2025-02-19 18:56:21 +0900

    Refactor file paths and imports

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,5 @@
-import 'reflect-metadata'
-
-export interface TestInterface {
-  name: string
-  value: number
-}
-
-export class TestClass {
-  constructor(private readonly data: TestInterface) {}
-
-  getData(): TestInterface {
-    return this.data
-  }
-}
+export * from './constants'
+export * from './decorators'
+export * from './generators'
+export * from './interfaces'
+export * from './utils'
