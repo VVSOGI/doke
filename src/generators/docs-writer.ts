23f91bf8e39ae1ded0e376363d530020c08ef9b9
benny <vvsogi@gmail.com>
2025-02-20 15:55:42 +0900

    Add DocsWriter class for generating documentation

---
+++ src/generators/docs-writer.ts
@@ -0,0 +1,29 @@
+import type { ApiController } from '../interfaces'
+import { FileManager } from '../utils'
+
+interface WriteData {
+  projectMetadata: unknown
+  controllers: ApiController[]
+}
+
+export class DocsWriter {
+  private readonly requiredDirectories = ['', 'routes']
+
+  constructor(private readonly fileManager: FileManager) {}
+
+  async writeDocumentation({ projectMetadata, controllers }: WriteData): Promise<void> {
+    for (const dir of this.requiredDirectories) {
+      const isExist = await this.fileManager.existsDirectory(dir)
+      if (!isExist) this.fileManager.createDirectory(dir)
+    }
+    await Promise.all([this.writeProjectMetadata(projectMetadata), this.writeControllerDocs(controllers)])
+  }
+
+  private async writeProjectMetadata(metadata: unknown): Promise<void> {
+    await this.fileManager.saveJson('projects', metadata)
+  }
+
+  private async writeControllerDocs(controllers: ApiController[]): Promise<void> {
+    await Promise.all(controllers.map((doc) => this.fileManager.saveJson(`routes/${doc.basePath}`, doc)))
+  }
+}
