7db1407da8a4a4988daeaf1035949ab447a27ae0
benny <vvsogi@gmail.com>
2025-02-21 16:43:11 +0900

    Add types for todo items

---
+++ src/__test__/__fixtures__/types/todo.types.ts
@@ -0,0 +1,16 @@
+export interface TodoItem {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface CreateTodoDto {
+  title: string
+}
+
+export interface UpdateTodoDto {
+  title?: string
+  completed?: boolean
+}
