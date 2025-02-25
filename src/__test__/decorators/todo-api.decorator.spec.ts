d28ed95343f618d8afc778e6e8b5fdf2b128ae7a
benny <vvsogi@gmail.com>
2025-02-25 15:50:00 +0900

    Add todo-api decorater test DocsCreateTodo

--- src/__test__/decorators/todo-api.decorator.spec.ts
+++ src/__test__/decorators/todo-api.decorator.spec.ts
@@ -1,10 +1,10 @@
 import { MetadataExtractor } from '../../utils'
 import { TodoController } from '../__fixtures__/controllers'
-import { getAllTodos } from '../__fixtures__/mocks/todo'
+import { createTodo, getAllTodos } from '../__fixtures__/mocks/todo'
 
 describe('Todo API Decorators', () => {
   describe('DocsGetAllTodos', () => {
-    it('should call ApiDocsEndpoint with correct metadata from mock', () => {
+    it('should call with correct metadata from mock', () => {
       const mock = getAllTodos
       const prototype = TodoController.prototype
       const methodNames = MetadataExtractor.extractMethodNames(prototype) as any[]
@@ -12,6 +12,20 @@ describe('Todo API Decorators', () => {
       const metadata = MetadataExtractor.extractEndpointMetadata(prototype, getAllTodosMethod)
 
       expect(metadata?.description).toEqual(mock.description)
+      expect(metadata?.response).toEqual(mock.response)
+    })
+  })
+
+  describe('DocsCreateTodo', () => {
+    it('should call with correct metadata from mock', () => {
+      const mock = createTodo
+      const prototype = TodoController.prototype
+      const methodNames = MetadataExtractor.extractMethodNames(prototype) as any[]
+      const createTodoMethod = methodNames.find((method) => method === 'createTodo')
+      const metadata = MetadataExtractor.extractEndpointMetadata(prototype, createTodoMethod)
+
+      expect(metadata?.description).toEqual(mock.description)
+      expect(metadata?.request).toEqual(mock.request)
       expect(metadata?.response).toEqual(mock.response)
     })
   })
