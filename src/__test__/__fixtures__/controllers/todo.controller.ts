71e78b7942ecf85f8c2552a757059864135cec9f
benny <vvsogi@gmail.com>
2025-02-25 15:55:06 +0900

    Refactor API methods in TodoController

--- src/__test__/__fixtures__/controllers/todo.controller.ts
+++ src/__test__/__fixtures__/controllers/todo.controller.ts
@@ -1,26 +1,30 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Controller } from '@nestjs/common'
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common'
 import { DocsTodoController, DocsGetAllTodos, DocsCreateTodo, DocsUpdateTodo, DocsDeleteTodo } from '../decorators'
 import { CreateTodoDto, TodoItem, UpdateTodoDto } from '../types'
 
 @Controller('todo')
 @DocsTodoController()
 export class TodoController {
+  @Get()
   @DocsGetAllTodos()
   getAllTodos(): TodoItem[] {
     return []
   }
 
+  @Post()
   @DocsCreateTodo()
   createTodo(dto: CreateTodoDto): TodoItem {
     return {} as TodoItem
   }
 
+  @Patch()
   @DocsUpdateTodo()
   updateTodo(id: string, dto: UpdateTodoDto): TodoItem {
     return {} as TodoItem
   }
 
+  @Delete()
   @DocsDeleteTodo()
   deleteTodo(id: string): { success: boolean } {
     return { success: true }
