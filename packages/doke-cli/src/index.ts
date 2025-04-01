aa227896d283b6782e9675661e9e7fa31cbedef3
benny <vvsogi@gmail.com>
2025-04-01 18:39:42 +0900

    Add doke-cli commands and update doke-ui .gitignore

---
+++ packages/doke-cli/src/index.ts
@@ -0,0 +1,24 @@
+import { Command } from 'commander'
+import chalk from 'chalk'
+
+const program = new Command()
+
+program.name('doke-cli').version('1.0.0', '-v, --version').description('Create relative doke just in time')
+
+program
+  .command('check-docker')
+  .description('Docker 설치 및 실행 상태 확인')
+  .action(async () => {
+    console.log(chalk.blue('Docker 환경 확인 중...'))
+  })
+
+program
+  .command('init')
+  .description('프로젝트 초기화 및 Docker 환경 확인')
+  .action(async () => {
+    console.log(chalk.blue('프로젝트 초기화 중...'))
+    console.log(chalk.blue('Docker 환경 확인 중...'))
+    console.log(chalk.green('✅ 프로젝트 초기화가 완료되었습니다.'))
+  })
+
+program.parse(process.argv)
