2e1072de349a106d1305ac2828cdc1f9a24fc0ec
benny <vvsogi@gmail.com>
2025-04-02 18:59:55 +0900

    Refactor git cloning process

--- packages/doke-cli/src/index.ts
+++ packages/doke-cli/src/index.ts
@@ -7,12 +7,13 @@ const program = new Command()
 program.name('doke-cli').version('1.0.0', '-v, --version').description('Create relative doke just in time')
 
 program
-  .command('create ui')
+  .command('create-ui')
   .description('Create doke ui just-in-time')
   .action(async () => {
     console.log(chalk.blue('Create doke ui'))
     const environment = await SelectCommand.chooseEnvironment()
-    GitCommand.cloneUIRepository()
+    const gitCommand = new GitCommand()
+    gitCommand.cloneUIRepository()
 
     if (environment === 'local') {
       console.log(chalk.blue('Set to local environment.'))
